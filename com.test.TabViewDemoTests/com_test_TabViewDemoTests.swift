45daa87d12d72441827c1c1ecca893c73d727764
bys <bys@bysdembp-2.guest.corp.microsoft.com>
2017-06-19 10:26:51 +0800

    Initial Commit

---
+++ com.test.TabViewDemoTests/com_test_TabViewDemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  com_test_TabViewDemoTests.swift
+//  com.test.TabViewDemoTests
+//
+//  Created by bys on 19/06/2017.
+//  Copyright © 2017 bys. All rights reserved.
+//
+
+import XCTest
+@testable import com_test_TabViewDemo
+
+class com_test_TabViewDemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
